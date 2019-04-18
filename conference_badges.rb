eaaf3a1b1a5a29d9c142a05060fcd9dae1f0d7a5
Aniket Patel <aniketupatel1@gmail.com>
2019-04-18 05:29:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |badge|
+    badges.push("Hello, my name is #{badge}.")
+  end 
